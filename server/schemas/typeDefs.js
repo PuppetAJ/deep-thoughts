8e1d82f8162937a0b4694004f0109bc75dc5a2db
Egg <adrianjimenez1950@gmail.com>
2022-11-28 00:29:47 -0500

    Basic mutations added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -9,12 +10,14 @@ const typeDefs = gql`
     reactionCount: Int
     reactions: [Reaction]
   }
+
   type Reaction {
     _id: ID
     reactionBody: String
     createdAt: String
     username: String
   }
+
   type User {
     _id: ID
     username: String
@@ -23,11 +26,26 @@ const typeDefs = gql`
     thoughts: [Thought]
     friends: [User]
   }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-
+const { authMiddleware } = require('./utils/auth');
 const {typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 
 const PORT = process.env.PORT || 3001;
