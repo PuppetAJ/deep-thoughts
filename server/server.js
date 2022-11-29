ea2f1a97fcb462c95197dfd4c12ca35017a55824
Egg <adrianjimenez1950@gmail.com>
2022-11-29 02:08:04 -0500

    added initial routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const {typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const path = require('path');
 
 const server = new ApolloServer({
   typeDefs,
@@ -15,6 +16,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
